f43ded77896ef0aed2c80b9e3b4d900ec000d8b3
Sasidhar Yendluri <sasi@Sasidhars-MacBook-Air.local>
2026-05-29 11:34:54 +0530

    feat(auth-service): register auth-service with eureka

---
+++ auth-service/auth-service/src/test/java/com/opencommerce/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opencommerce.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
